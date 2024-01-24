c0050c9e5e2e49105fe6536c73cc05794730a7fa
alessandro <abiank@users.noreply.github.com>
2024-01-24 10:09:28 +0100

    downgrade to 2

--- modules/config.py
+++ modules/config.py
@@ -256,7 +256,7 @@ default_image_number = get_config_item_or_set_default(
 checkpoint_downloads = get_config_item_or_set_default(
     key='checkpoint_downloads',
     default_value={
-        "juggernautXL_version6Rundiffusion.404": "https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/juggernautXL_version6Rundiffusion.safetensors.404"
+        "juggernautXL_2.safetensors": "https://civitai.com/api/download/models/152076"
     },
     validator=lambda x: isinstance(x, dict) and all(isinstance(k, str) and isinstance(v, str) for k, v in x.items())
 )
